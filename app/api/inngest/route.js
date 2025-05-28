8fe6a09a35bea6eede047bb9b81f2b9244e2fa6b
Hezejack <Hezejack1@gmail.com>
2025-05-28 17:02:49 +0100

    big12

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,25 +1,3 @@
-// import { serve } from "inngest/next";
-
-// // import { inngest } from "../../../inngest/client";
-// import {
-//   //   createUserOrder,
-//   inngest,
-//   syncUserCreation,
-//   syncUserDeletion,
-//   syncUserUpdation,
-// } from "@/config/inngest";
-
-// // Create an API that serves zero functions
-// export const { GET, POST, PUT } = serve({
-//   client: inngest,
-//   functions: [
-//     syncUserCreation,
-//     syncUserDeletion,
-//     syncUserUpdation,
-//     // createUserOrder /* your functions will be passed here later! */,
-//   ],
-// });
-
 import { serve } from "inngest/next";
 import {
   inngest,
--- models/User.js
+++ models/User.js
@@ -1,20 +1,3 @@
-// import mongoose from "mongoose";
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     _id: { type: String, required: true },
-//     name: { type: String, required: true },
-//     email: { type: String, required: true, unique: true },
-//     imageUrl: { type: String, required: true },
-//     CartItems: { type: Object, default: {} },
-//   },
-//   { minimize: false }
-// );
-
-// const User = mongoose.model.user || mongoose.model("user", userSchema);
-
-// export default User;
-
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
